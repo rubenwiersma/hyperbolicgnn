a9307c79b4d742fc9283e46d8b85b29ea6813431
rubenwiersma <r.t.wiersma@tudelft.nl>
2021-12-02 21:43:57 +0100

    Update config files

--- experiments/table_1.sh
+++ experiments/table_1.sh
@@ -1,35 +1,35 @@
 python experiments/train_synthetic.py --config experiments/configs/synth_euclidean.yaml --log_timestamp table1 --embed_dim 3
 python experiments/train_synthetic.py --config experiments/configs/synth_euclidean.yaml --log_timestamp table1 --embed_dim 5
 python experiments/train_synthetic.py --config experiments/configs/synth_euclidean.yaml --log_timestamp table1 --embed_dim 10
-# python experiments/train_synthetic.py --config experiments/configs/synth_euclidean.yaml --log_timestamp table1 --embed_dim 20
-# python experiments/train_synthetic.py --config experiments/configs/synth_euclidean.yaml --log_timestamp table1 --embed_dim 256
+python experiments/train_synthetic.py --config experiments/configs/synth_euclidean.yaml --log_timestamp table1 --embed_dim 20
+python experiments/train_synthetic.py --config experiments/configs/synth_euclidean.yaml --log_timestamp table1 --embed_dim 256
 
 python experiments/train_synthetic.py --config experiments/configs/synth_poincare.yaml --log_timestamp table1 --embed_dim 3
 python experiments/train_synthetic.py --config experiments/configs/synth_poincare.yaml --log_timestamp table1 --embed_dim 5
 python experiments/train_synthetic.py --config experiments/configs/synth_poincare.yaml --log_timestamp table1 --embed_dim 10
-# python experiments/train_synthetic.py --config experiments/configs/synth_poincare.yaml --log_timestamp table1 --embed_dim 20
-# python experiments/train_synthetic.py --config experiments/configs/synth_poincare.yaml --log_timestamp table1 --embed_dim 256
+python experiments/train_synthetic.py --config experiments/configs/synth_poincare.yaml --log_timestamp table1 --embed_dim 20
+python experiments/train_synthetic.py --config experiments/configs/synth_poincare.yaml --log_timestamp table1 --embed_dim 256
 
 python experiments/train_synthetic.py --config experiments/configs/synth_lorentz.yaml --log_timestamp table1 --embed_dim 3
 python experiments/train_synthetic.py --config experiments/configs/synth_lorentz.yaml --log_timestamp table1 --embed_dim 5
 python experiments/train_synthetic.py --config experiments/configs/synth_lorentz.yaml --log_timestamp table1 --embed_dim 10
-# python experiments/train_synthetic.py --config experiments/configs/synth_lorentz.yaml --log_timestamp table1 --embed_dim 20
-# python experiments/train_synthetic.py --config experiments/configs/synth_lorentz.yaml --log_timestamp table1 --embed_dim 256
+python experiments/train_synthetic.py --config experiments/configs/synth_lorentz.yaml --log_timestamp table1 --embed_dim 20
+python experiments/train_synthetic.py --config experiments/configs/synth_lorentz.yaml --log_timestamp table1 --embed_dim 256
 
 python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 3
 python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 5
 python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 10
-# python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20
-# python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256
 
 python experiments/evaluate_synthetic.py --config experiments/configs/synth_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 3
 python experiments/evaluate_synthetic.py --config experiments/configs/synth_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 5
 python experiments/evaluate_synthetic.py --config experiments/configs/synth_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 10
-# python experiments/evaluate_synthetic.py --config experiments/configs/synth_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20
-# python experiments/evaluate_synthetic.py --config experiments/configs/synth_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256
 
 python experiments/evaluate_synthetic.py --config experiments/configs/synth_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 3
 python experiments/evaluate_synthetic.py --config experiments/configs/synth_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 5
 python experiments/evaluate_synthetic.py --config experiments/configs/synth_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 10
-# python experiments/evaluate_synthetic.py --config experiments/configs/synth_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20
-# python experiments/evaluate_synthetic.py --config experiments/configs/synth_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256
